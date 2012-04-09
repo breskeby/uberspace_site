7eda0c1223b46c12a7e61d67f164bc5f707dcb01
Rene Groeschke <rene@breskeby.com>
2012-04-10 01:20:19 +0200

    initial commit (rails skeleton / not modified)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UberspaceSite::Application.config.secret_token = 'eb29ca7977887570fe7b5ba7f0e9425c7f849da40c94c4c7e8cd8d4a5230429de978cc822c0bf6158dfc832f35894dd55440adb4dc12a78bb33ed44548511908'
